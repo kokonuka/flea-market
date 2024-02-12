db5a923c5ba06b2fe3e6c2cbea5ca832fe81d4b1
kokonuka <miuraasahi1@gmail.com>
2024-02-12 14:35:03 +0900

    マイグレーションのタイムスタンプを変更

--- src/migrations/1636250494707-CreateItem.ts
+++ src/migrations/1636250494707-CreateItem.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class CreateItem1707355795341 implements MigrationInterface {
-    name = 'CreateItem1707355795341'
+export class CreateItem1636250494707 implements MigrationInterface {
+    name = 'CreateItem1636250494707'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "item" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "name" character varying NOT NULL, "price" integer NOT NULL, "description" character varying NOT NULL, "status" character varying NOT NULL, "createdAt" character varying NOT NULL, "updatedAt" character varying NOT NULL, CONSTRAINT "PK_d3c0c71f23e7adcf952a1d13423" PRIMARY KEY ("id"))`);
